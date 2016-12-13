336aa1d94f8d1a850a3bced470909faa5ad53d49
Sharvy Ahmed <sharvy2008@gmail.com>
2016-12-14 04:19:35 +0600

    Renamed App.

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CrossoverPOS
+module EasyPOS
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
     config.react.addons = true
